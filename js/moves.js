bc36cdbbed39ce7e941705bf7b783d7e3c8ee1ff
Mustafa Uysal <41873800+mustafauyysl@users.noreply.github.com>
2021-02-21 04:20:19 +0300

    add-pawn-moves

--- js/moves.js
+++ js/moves.js
@@ -1,14 +1,48 @@
 // TODO
 
+let selectedPiece = "";
+
+const clearMove = () => {
+  for (let i = 0; i <= squares.length; i++) {
+    squares[i].classList.remove("green");
+  }
+};
+
+const moveBlackPawn = (x) => {
+  if (!squares[x + 8].id) {
+    squares[x + 8].classList.toggle("green");
+    squares[x + 16].classList.toggle("green");
+  }
+  if (squares[x + 7].id) {
+    squares[x + 7].classList.toggle("green");
+  }
+  if (squares[x + 9].id) {
+    squares[x + 9].classList.toggle("green");
+  }
+};
+
+const moveWhitePawn = (x) => {
+  if (!squares[x - 8].id) {
+    squares[x - 8].classList.toggle("green");
+    squares[x - 16].classList.toggle("green");
+  }
+};
+
 for (let i = 0; i < squares.length; i++) {
   squares[i].addEventListener("click", () => {
     if (squares[i].id === "♟︎") {
-      squares[i + 8].classList.toggle("green");
-      squares[i + 16].classList.toggle("green");
+      moveBlackPawn(i);
+      selectedPiece = squares[i];
     }
     if (squares[i].id === "♙") {
-      squares[i - 8].classList.toggle("green");
-      squares[i - 16].classList.toggle("green");
+      moveWhitePawn(i);
+      selectedPiece = squares[i];
+    }
+    if (squares[i].classList.contains("green")) {
+      placePiece(squares[i], { title: selectedPiece.id });
+      selectedPiece.innerHTML = "";
+      selectedPiece.removeAttribute("id");
+      clearMove();
     }
   });
 }
